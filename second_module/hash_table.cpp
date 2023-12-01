c31312d6901741f67dc2585cb3d87f8e3db794ff
MrDzhofik <djafarovemil04@mail.ru>
2023-12-01 03:14:16 +0300

    Solved first and second task, made more beatiful code

--- second_module/hash_table.cpp
+++ second_module/hash_table.cpp
@@ -50,145 +50,17 @@ public:
     }
 
     // Adding
-    bool Add(const T &key)
-    {
-        // если коэффициент заполнения >  3/4
-        if (4 * size >= tableSize * 3)
-        {
-            grow();
-        }
-
-        size_t first = hasherOne(key);
-        bool del = true;
-        int index = -1;
-        size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
-        size_t hash = first % tableSize;
-
-        // поиск добавляемой ячейки
-        for (int i = 1; i <= tableSize; ++i)
-        {
-            if (table[hash] == key)
-            {
-                return false;
-            }
-            if (table[hash] == DELETED && del)
-            {
-                del = false;
-                index = hash; // запоминаем первую удаленную ячейку
-            }
-            if (table[hash] == EMPTY)
-            {
-                break;
-            }
-            hash = (first + i * second) % tableSize; // двойное хэширование
-        }
-        // eсли не нашли удаленных ячеек, то записываем в пустую if (index == -1)
-        {
-            index = hash;
-        }
-        // если ячейка не удаленная и не пустая, мы сделали кол-во пробирований = количество ячеек в таблице
-        if (table[index] != DELETED && table[index] != EMPTY)
-        {
-            // grow();
-            int i = 0;
-            index = first % tableSize;
-            while (table[index] != DELETED && table[index] != EMPTY && i < tableSize)
-            {
-                ++i;
-                index = (first + i * second) % tableSize + 1; // пробирование двойным хэшированием
-            }
-        }
-        table[index] = key;
-        if (del)
-        {
-            ++size;
-        }
-        return true;
-    }
+    bool Add(const T &key);
 
     // Finding
-    bool Has(const T &key)
-    {
-        size_t first = hasherOne(key);
-        size_t hash = first % tableSize;
-
-        size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
-        for (int i = 1; i <= tableSize; ++i)
-        {
-            if (table[hash] == key)
-            {
-                return true;
-            }
-            if (table[hash] == EMPTY)
-            {
-                return false;
-            }
-            hash = (first + i * second) % tableSize; // двойное хэширование
-        }
-        return false;
-    }
+    bool Has(const T &key);
 
     // Deleting
-    bool Delete(const T &key)
-    {
-        size_t first = hasherOne(key);
-        size_t hash = first % tableSize;
-
-        size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
-        for (int i = 1; i <= tableSize; ++i)
-        {
-            if (table[hash] == key)
-            {
-                table[hash] = DELETED;
-                return true;
-            }
-            if (table[hash] == EMPTY)
-            {
-                break;
-            }
-            hash = (first + i * second) % tableSize;
-        }
-        return false;
-    }
+    bool Delete(const T &key);
 
 private:
     // rehash
-    void grow()
-    {
-        tableSize *= 2;
-        std::vector<T> newTable(tableSize, EMPTY);
-
-        for (int i = 0; i < tableSize / 2; ++i)
-        {
-            if (table[i] != EMPTY)
-            {
-                // удаленные ячейки не заносим в новую таблицу, поэтому уменьшаем размер
-                if (table[i] == DELETED)
-                {
-                    --size;
-                }
-                // иначе ищем место для ячейки в новой таблице
-                else
-                {
-                    size_t firstHash = hasherOne(table[i]);
-                    size_t hash = firstHash % tableSize;
-                    if (newTable[hash] != EMPTY)
-                    {
-                        int j = 0;
-                        size_t secondHash = hasherTwo(table[i]) * 2 + 1;
-                        while (newTable[hash] != EMPTY)
-                        {
-                            ++j;
-                            hash = (firstHash + j * secondHash) % tableSize;
-                        }
-                    }
-                    newTable[hash] = table[i];
-                }
-            }
-        }
-
-        table = std::move(newTable);
-    }
+    void grow();
 
     std::vector<T> table;
     size_t size;
@@ -196,6 +68,147 @@ private:
     Hasher hasherOne;
     Hasher hasherTwo{37};
 };
+
+template <typename T, typename Hasher>
+bool HashTable<T, Hasher>::Add(const T &key)
+{
+    // если коэффициент заполнения >  3/4
+    if (4 * size >= tableSize * 3)
+    {
+        grow();
+    }
+
+    size_t first = hasherOne(key);
+    bool del = true;
+    int index = -1;
+    size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
+    size_t hash = first % tableSize;
+
+    // поиск добавляемой ячейки
+    for (int i = 1; i <= tableSize; ++i)
+    {
+        if (table[hash] == key)
+        {
+            return false;
+        }
+        if (table[hash] == DELETED && del)
+        {
+            del = false;
+            index = hash; // запоминаем первую удаленную ячейку
+        }
+        if (table[hash] == EMPTY)
+        {
+            break;
+        }
+        hash = (first + i * second) % tableSize; // двойное хэширование
+    }
+    // eсли не нашли удаленных ячеек, то записываем в пустую
+    if (index == -1)
+    {
+        index = hash;
+    }
+    // если ячейка не удаленная и не пустая, мы сделали кол-во пробирований = количество ячеек в таблице
+    if (table[index] != DELETED && table[index] != EMPTY)
+    {
+        grow();
+        int i = 0;
+        index = first % tableSize;
+        while (table[index] != DELETED && table[index] != EMPTY && i < tableSize)
+        {
+            ++i;
+            index = (first + i * second) % tableSize; // пробирование двойным хэшированием
+        }
+    }
+    table[index] = key;
+    if (del)
+    {
+        ++size;
+    }
+    return true;
+}
+
+template <typename T, typename Hasher>
+bool HashTable<T, Hasher>::Delete(const T &key)
+{
+    size_t first = hasherOne(key);
+    size_t hash = first % tableSize;
+
+    size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
+    for (int i = 1; i <= tableSize; ++i)
+    {
+        if (table[hash] == key)
+        {
+            table[hash] = DELETED;
+            return true;
+        }
+        if (table[hash] == EMPTY)
+        {
+            break;
+        }
+        hash = (first + i * second) % tableSize;
+    }
+    return false;
+}
+
+template <typename T, typename Hasher>
+bool HashTable<T, Hasher>::Has(const T &key)
+{
+    size_t first = hasherOne(key);
+    size_t hash = first % tableSize;
+
+    size_t second = hasherTwo(key) * 2 + 1; // h2 должна возвращать нечетные значения
+    for (int i = 1; i <= tableSize; ++i)
+    {
+        if (table[hash] == key)
+        {
+            return true;
+        }
+        if (table[hash] == EMPTY)
+        {
+            return false;
+        }
+        hash = (first + i * second) % tableSize; // двойное хэширование
+    }
+    return false;
+}
+
+template <typename T, typename Hasher>
+void HashTable<T, Hasher>::grow()
+{
+    tableSize *= 2;
+    std::vector<T> newTable(tableSize, EMPTY);
+
+    for (int i = 0; i < tableSize / 2; ++i)
+    {
+        if (table[i] != EMPTY)
+        {
+            // удаленные ячейки не заносим в новую таблицу, поэтому уменьшаем размер
+            if (table[i] == DELETED)
+            {
+                --size;
+            }
+            // иначе ищем место для ячейки в новой таблице
+            else
+            {
+                size_t firstHash = hasherOne(table[i]);
+                size_t hash = firstHash % tableSize;
+                if (newTable[hash] != EMPTY)
+                {
+                    int j = 0;
+                    size_t secondHash = hasherTwo(table[i]) * 2 + 1;
+                    while (newTable[hash] != EMPTY)
+                    {
+                        ++j;
+                        hash = (firstHash + j * secondHash) % tableSize;
+                    }
+                }
+                newTable[hash] = table[i];
+            }
+        }
+    }
+
+    table = std::move(newTable);
+}
 
 void doLogic(std::istream &in, std::ostream &out)
 {
--- second_module/pre_order.cpp
+++ second_module/pre_order.cpp
@@ -12,124 +12,255 @@
 #include <vector>
 #include <stack>
 
+template <class T>
+struct Node
+{
+    Node() : left(nullptr), right(nullptr), visited(false){};
+    Node(T &data) : left(nullptr), right(nullptr), visited(false), deleted(false), value(data){};
+    ~Node(){
+        // delete left;
+        // delete right;
+    };
+
+    bool is_visited()
+    {
+        return visited;
+    }
+
+    void visit()
+    {
+        visited = true;
+    }
+
+    void delet()
+    {
+        deleted = true;
+    }
+
+    T value;
+    bool deleted;
+    bool visited;
+    Node *left;
+    Node *right;
+};
+
+template <class T>
+struct DefaultComparator
+{
+    bool operator()(const T &l, const T &r) const
+    {
+        return l > r;
+    }
+};
+
+class NodeComparator
+{
+public:
+    NodeComparator()
+    {
+    }
+
+    bool operator()(const Node<int> *l, const Node<int> *r)
+    {
+        return l->value >= r->value;
+    }
+};
+
 // B-Tree
-template <typename T>
+template <typename T, typename Comparator = DefaultComparator<int>>
 class BTree
 {
 public:
-    struct Node
-    {
-        Node() : left(nullptr), right(nullptr), visited(false){};
-        Node(T &data) : left(nullptr), right(nullptr), visited(false), value(data){};
-        ~Node()
-        {
-            delete left;
-            delete right;
-        };
-
-        bool is_visited()
-        {
-            return visited;
-        }
-
-        void visit()
-        {
-            visited = true;
-        }
-
-        T value;
-        bool visited;
-        Node *left;
-        Node *right;
-    };
     BTree() : root(nullptr){};
     BTree(int size) : root(nullptr), size(size){};
-    ~BTree(){};
+    ~BTree();
 
     // Insert
-    void Insert(T data)
-    {
-        Node *tmp = new Node(data);
-        bool find = false;
-        // если еще нет элементов
-        if (!root)
-        {
-            root = tmp;
-        }
-        else
-        {
-            Node *p = root;
-            while (!find)
-            {
-                if (p->value > data)
-                {
-                    if (!p->left)
-                    {
-                        find = true;
-                        p->left = tmp;
-                    }
-                    p = p->left;
-                }
-                else if (p->value < data)
-                {
-                    if (!p->right)
-                    {
-                        find = true;
-                        p->right = tmp;
-                    }
-                    p = p->right;
-                }
-            }
-        }
-    }
+    void Insert(T &data);
 
     // Print
-    std::vector<int> PrintPreOrder()
-    {
-        std::stack<Node *> st;
-        Node *p = root;
-        int i = 0;
-        std::vector<int> result(size, 0);
-        result[i++] = p->value;
-        st.push(p);
-        while (i != size)
-        {
-            if (p->left && !p->left->is_visited())
-            {
-                p = p->left;
-                p->visit();
-                result[i] = p->value;
-                st.push(p);
-                ++i;
-            }
-            else if (p->right && !p->right->is_visited())
-            {
-                p = p->right;
-                p->visit();
-                result[i] = p->value;
-                st.push(p);
-                ++i;
-            }
-            else
-            {
-                p = st.top();
-                st.pop();
-            }
-        }
-        return result;
-    };
+    std::vector<int> PrintPreOrder();
 
 private:
     size_t size;
-    Node *root;
+    Node<int> *root;
+    Comparator cmp;
 };
+
+template <typename T, typename Comparator>
+void BTree<T, Comparator>::Insert(T &data)
+{
+    Node<int> *tmp = new Node(data);
+    bool find = false;
+    // если еще нет элементов
+    if (!root)
+    {
+        root = tmp;
+    }
+    else
+    {
+        Node<int> *p = root;
+        while (!find)
+        {
+            if (cmp(p, tmp))
+            {
+                if (!p->left)
+                {
+                    find = true;
+                    p->left = tmp;
+                }
+                p = p->left;
+            }
+            else
+            {
+                if (!p->right)
+                {
+                    find = true;
+                    p->right = tmp;
+                }
+                p = p->right;
+            }
+        }
+    }
+}
+
+template <typename T, typename Comparator>
+std::vector<int> BTree<T, Comparator>::PrintPreOrder()
+{
+    std::stack<Node<int> *> st;
+    Node<int> *p = root;
+    int i = 0;
+    std::vector<int> result(size, 0);
+    result[i++] = p->value;
+    st.push(p);
+    while (i != size)
+    {
+        if (p->left && !p->left->is_visited())
+        {
+            p = p->left;
+            p->visit();
+            result[i] = p->value;
+            st.push(p);
+            ++i;
+        }
+        else if (p->right && !p->right->is_visited())
+        {
+            p = p->right;
+            p->visit();
+            result[i] = p->value;
+            st.push(p);
+            ++i;
+        }
+        else
+        {
+            p = st.top();
+            st.pop();
+        }
+    }
+    return result;
+};
+
+template <typename T, typename Comparator>
+BTree<T, Comparator>::~BTree()
+{
+    // std::stack<Node<int> *> st;
+    // Node<int> *p = root;
+    // st.push(p);
+    // while (!st.empty())
+    // {
+    //     if (p->left || p->right)
+    //     {
+    //         if (p->left->left || p->left->right)
+    //         {
+    //             st.push(p);
+    //             p = p->left;
+    //         }
+    //         else if (p->right->left || p->right->right)
+    //         {
+    //             st.push(p);
+    //             p = p->right;
+    //         }
+    //         else
+    //         {
+    //             delete p;
+    //             p = st.top();
+    //             st.pop();
+    //         }
+    //     }
+    // }
+
+    std::stack<Node<int> *> st;
+    Node<int> *p = root;
+    int i = 0;
+    if (p->left && p->right)
+    {
+        st.push(p);
+        st.push(p);
+    }
+    else if (p->left || p->right)
+    {
+        st.push(p);
+    }
+    while (!st.empty())
+    {
+        if (p->left && !p->left->deleted)
+        {
+            p = p->left;
+            if (p->left && p->right)
+            {
+                st.push(p);
+                st.push(p);
+            }
+            else if (p->left || p->right)
+            {
+                st.push(p);
+            }
+            else
+            {
+                delete p;
+                p = st.top();
+                st.pop();
+                p->left->deleted = true;
+            }
+            ++i;
+        }
+        else if (p->right && !p->right->deleted)
+        {
+            p = p->right;
+            if (p->left && p->right)
+            {
+                st.push(p);
+                st.push(p);
+            }
+            else if (p->left || p->right)
+            {
+                st.push(p);
+            }
+            else
+            {
+                delete p;
+                p = st.top();
+                st.pop();
+                p->right->deleted = true;
+            }
+        }
+        else
+        {
+            delete p;
+            p->deleted = true;
+            p = st.top();
+            st.pop();
+        }
+    }
+    delete root;
+}
 
 int main()
 {
     int n = 0;
-    int data = 0;
     std::cin >> n;
-    BTree<int> tree(n);
+    BTree<int, NodeComparator> tree(n);
+    int data = 0;
     for (int i = 0; i < n; ++i)
     {
         std::cin >> data;
